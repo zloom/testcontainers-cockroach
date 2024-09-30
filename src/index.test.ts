4e5fe6316cc85d26ebeeba6f53ce7b2df74fa5c2
Anton Zalialdinov <zloomsky@gmail.com>
2024-09-30 14:24:50 +0200

    tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,43 @@
 import { v4 } from 'uuid'
+import { CockroachContainer } from './index'
+import { Client } from 'pg'
 
-test('Tool produces output script', async () => {
-  const id = v4()
-  expect(true).toBe(true)
+test('Cockroach container can start witn memory storage', async () => {
+  const container = await new CockroachContainer();
+  container.withMemoryStorage();
+
+  const startedContainer = await container.start();
+  const connectionString = startedContainer.getConnectionUri();
+
+  const client = new Client(connectionString);
+  client.connect();
+
+  await client.query('CREATE TABLE accounts (id UUID PRIMARY KEY, balance INT8)');
+  await client.query(`INSERT INTO accounts (id, balance) VALUES ($1, 1000), ($2, 250), ($3, 0)`, [v4(), v4(), v4()]);
+  const accountsResult = await client.query('SELECT id, balance FROM accounts ORDER BY balance')
+
+  expect(accountsResult.rows).toMatchObject([{ balance: '0' }, { balance: '250' }, { balance: '1000' }])
+
+  await client.end();
+  await startedContainer.stop();
+})
+
+test('Cockroach container can start with custom db name', async () => {
+  const container = await new CockroachContainer();
+  container.withDatabase('testsdb');
+
+  const startedContainer = await container.start();
+  const connectionString = startedContainer.getConnectionUri();
+
+  const client = new Client(connectionString);
+  client.connect();
+
+  await client.query('CREATE TABLE accounts (id UUID PRIMARY KEY, balance INT8)');
+  await client.query(`INSERT INTO accounts (id, balance) VALUES ($1, 1000), ($2, 250), ($3, 0)`, [v4(), v4(), v4()]);
+  const accountsResult = await client.query('SELECT id, balance FROM accounts ORDER BY balance')
+  
+  expect(accountsResult.rows).toMatchObject([{ balance: '0' }, { balance: '250' }, { balance: '1000' }])
+
+  await client.end();
+  await startedContainer.stop();
 })
--- src/index.ts
+++ src/index.ts
@@ -5,41 +5,77 @@ import {
   Wait,
 } from 'testcontainers'
 
-const DB_PORT = 26257
-const REST_PORT = 8080
-
-export class StartedCockroachContainer extends AbstractStartedContainer {
-  private readonly port: string
-
-  constructor(startedTestContainer: StartedTestContainer) {
-    super(startedTestContainer)
-    this.port = startedTestContainer.getMappedPort(DB_PORT).toString()
-  }
-
-  public getPort(): string {
-    return this.port
-  }
-}
+const COCKROACH_PORT = 26257
+const COCKROACH_REST_PORT = 8080
+const COCKROACH_USERNAME = 'root'
 
 export class CockroachContainer extends GenericContainer {
+  private database = "defaultdb";
+  private storageType = "cockroach-data";
+
   constructor(image = 'cockroachdb/cockroach:v23.1.4') {
     super(image)
-    this.withExposedPorts(DB_PORT, REST_PORT)
+    this
+      .withExposedPorts(COCKROACH_PORT, COCKROACH_REST_PORT)
       .withWaitStrategy(
-        Wait.forHttp('/health?ready=1', REST_PORT)
+        Wait.forHttp('/health?ready=1', COCKROACH_REST_PORT)
           .forStatusCode(200)
           .withStartupTimeout(120_000),
       )
-      .withCommand([
-        'start-single-node',
-        '--insecure',
-        '--store=type=mem,size=0.25',
-      ])
-      .withReuse()
+  }
+
+  public withDatabase(database: string): this {
+    this.database = database;
+    return this;
+  }
+
+  public withMemoryStorage(): this {
+    this.storageType = 'type=mem,size=0.25'
+    return this;
   }
 
   public override async start(): Promise<StartedCockroachContainer> {
-    const started = await super.start()
-    return new StartedCockroachContainer(started)
+    this.withEnvironment({
+      COCKROACH_DATABASE: this.database,
+    });
+
+    this.withCommand([
+      'start-single-node',
+      '--insecure',
+      `--store=${this.storageType}`,
+    ])
+
+    return new StartedCockroachContainer(await super.start(), this.database)
+  }
+}
+
+export class StartedCockroachContainer extends AbstractStartedContainer {
+  private readonly port: number;
+
+  constructor(startedTestContainer: StartedTestContainer,
+    private readonly database: string,
+  ) {
+    super(startedTestContainer)
+    this.port = startedTestContainer.getMappedPort(COCKROACH_PORT)
+  }
+
+  public getPort(): number {
+    return this.port;
+  }
+
+  public getDatabase(): string {
+    return this.database;
+  }
+
+  /**
+  * @returns A connection URI in the form of `postgres[ql]://[username[:password]@][host[:port],]/database`
+  */
+  public getConnectionUri(): string {
+    const url = new URL("", "postgres://");
+    url.hostname = this.getHost();
+    url.port = this.getPort().toString();
+    url.pathname = this.getDatabase();
+    url.username = COCKROACH_USERNAME;
+    return url.toString();
   }
 }
